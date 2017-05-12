30bb60064939cc6863ebb2801f64118bbd3fe5be
MattYoshida <mjayyoshida@gmail.com>
2017-05-11 20:39:13 -0700

    Delete Magic8Ball.py

--- Magic8Ball.py
+++
@@ -1,8 +0,0 @@
-import random
-
-answers = ["It is certain", "Go away, I do not wish to answer at this time!", "Without a doubt", "Ask Again Later", "Better not tell you now", "Dont count on it", "Outlook not so good"]
-
-question = raw_input("Hi im the Magic 8 Ball, ask me a question >> ")
-
-rand = random.randint(0,len(answers)-1)
-print answers[rand]
